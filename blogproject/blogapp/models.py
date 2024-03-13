ec746a994476915e5e1cc4cc2360c4a5bec2fb53
tgg <gatheruteving@gmail.com>
2024-03-13 15:27:12 +0300

    add models

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
+    date_registered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='blogs')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='blogs')
+    article = models.TextField()
+    article_title = models.CharField(max_length=255)
+    date_of_publish = models.DateField()
+
+    def __str__(self):
+        return self.article_title
--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from blogproject.blogapp.models import Author, Blog, Category
+
 # Create your views here.
+def index(request):
+    context = {
+        'all_authors' : Author.objects.all(),
+        'all_blogs'  : Blog.objects.all(),
+        'all_categories'  : Category.objects.all(),
+    }
+    return render(request, 'blogapp/index.html')
